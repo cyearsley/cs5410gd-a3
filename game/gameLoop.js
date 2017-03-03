d41217abca187f5d96ef3e83f9e75ea95d28b276
Caleb Yearsley <cnyearsley@outlook.com>
2017-03-03 01:08:25 -0700

    Continue setting up the framework of the game.

--- game/gameLoop.js
+++ game/gameLoop.js
@@ -1,13 +1,23 @@
 var GameLoop = function () {
-    function _gameLoop () {
 
+    var sceneControl = new MasterScene();
+    this.init = function () {
+        window.requestAnimationFrame(_gameLoop);
+    };
+
+    function _gameLoop (timestamp) {
+        _update();
+        sceneControl.handleInput();
+        _render();
+
+        window.requestAnimationFrame(_gameLoop);
     }
 
     function _update () {
-
+        sceneControl.update();
     }
 
     function _render () {
-
+        sceneControl.render();
     };
 };
--- scenes/MasterScene.js
+++ scenes/MasterScene.js
@@ -1,3 +1,104 @@
 var MasterScene = function () {
 
+    // ======================================================= //
+    //
+    // C A N V A S - S P E C I F I C S
+    //
+    // ======================================================= //
+    var canvas = $('#canvas-main')[0];
+    var context = canvas.getContext('2d');
+    CanvasRenderingContext2D.prototype.clear = function() {
+        this.save();
+        this.setTransform(1, 0, 0, 1, 0, 0);
+        this.clearRect(0, 0, canvas.width, canvas.height);
+        this.restore();
+    };
+    this.init = function () {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    };
+    this.beginRender = function () {
+        context.clear();
+    };
+    // ======================================================= //
+
+    var sceneMain = function () {
+        var characters = [];
+        this.init = function () {
+
+        };
+        this.renderScene = function () {};
+        this.updateScene = function () {};
+        this.handleInputScene = function () {console.log("handle input")};
+    };
+
+    var scenePause = function () {
+        var characters = [];
+        this.init = function () {
+
+        };
+        this.renderScene = function () {};
+        this.updateScene = function () {};
+        this.handleInputScene = function () {};
+    };
+
+    var scenePlay = function () {
+        var characters = [];
+        this.init = function () {
+
+        };
+        this.renderScene = function () {};
+        this.updateScene = function () {};
+        this.handleInputScene = function () {};
+    };
+
+    var sceneHiscores = function () {
+        var characters = [];
+        this.init = function () {
+
+        };
+        this.renderScene = function () {};
+        this.updateScene = function () {};
+        this.handleInputScene = function () {};
+    };
+
+    var scenes = {
+        main: new sceneMain(),
+        pause: new scenePause(),
+        play: new scenePlay(),
+        hiscores: new sceneHiscores(),
+        currentScene: 'main'
+    };
+
+    this.render = function () {
+        // Render the scene, and all the characters in it.
+        for (key in scenes) {
+            if (scenes[key] && key != 'currentScene') {
+                if (key === scenes.currentScene) {
+                    scenes[key].renderScene();
+                }
+            }
+        }
+    };
+
+    this.update = function () {
+        // Update the scene, and all the characters in it.
+        for (key in scenes) {
+            if (scenes[key] && key != 'currentScene') {
+                if (key === scenes.currentScene) {
+                    scenes[key].updateScene();
+                }
+            }
+        }
+    };
+
+    this.handleInput = function () {
+        // Handle the input for the scene.
+        for (key in scenes) {
+            if (scenes[key] && key != 'currentScene') {
+                if (key === scenes.currentScene) {
+                    scenes[key].handleInputScene();
+                }
+            }
+        }
+    };
 };
